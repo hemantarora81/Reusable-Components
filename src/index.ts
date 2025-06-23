4c91c7cac6a15ce52d1d5b8bf72e9da5782bddc9
Hemant Arora <arorahemant75@gmail.com>
2025-06-23 17:22:06 +0530

    Index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-import ToggleComponent from "./commonComponents/ToggleComponent"
-import CheckBoxComponent from "./commonComponents/CheckBoxComponent"
-import DropdownComponent from "./commonComponents/DropdownComponent"
-import GridComponent from "./commonComponents/GridComponent"
-import MultiSelectComponent from "./commonComponents/MultiSelectComponent"
-import TextAreaComponent from "./commonComponents/TextAreaComponent"
-import TextBoxComponent from "./commonComponents/TextBoxComponent"
+// src/index.ts
+
+import ToggleComponent from "./commonComponents/ToggleComponent";
+import CheckBoxComponent from "./commonComponents/CheckBoxComponent";
+import DropdownComponent from "./commonComponents/DropdownComponent";
+import GridComponent from "./commonComponents/GridComponent";
+import MultiSelectComponent from "./commonComponents/MultiSelectComponent";
+import TextAreaComponent from "./commonComponents/TextAreaComponent";
+import TextBoxComponent from "./commonComponents/TextBoxComponent";
+
+// Re-exporting all components
+export {
+  ToggleComponent,
+  CheckBoxComponent,
+  DropdownComponent,
+  GridComponent,
+  MultiSelectComponent,
+  TextAreaComponent,
+  TextBoxComponent,
+};
