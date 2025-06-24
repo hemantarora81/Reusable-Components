372df46b195f84048a61d0c086808bc189bf7d0a
Hemant Arora <arorahemant75@gmail.com>
2025-06-24 15:16:26 +0530

    Updates

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
-import { default as ToggleComponent } from './commonComponents/ToggleComponent';
-import { default as CheckBoxComponent } from './commonComponents/CheckBoxComponent';
-import { default as DropdownComponent } from './commonComponents/DropdownComponent';
-import { default as GridComponent } from './commonComponents/GridComponent';
-import { default as MultiSelectComponent } from './commonComponents/MultiSelectComponent';
-import { default as TextAreaComponent } from './commonComponents/TextAreaComponent';
-import { default as TextBoxComponent } from './commonComponents/TextBoxComponent';
+import { default as ToggleComponent } from './commonComponents/ToggleComponent/index';
+import { default as CheckBoxComponent } from './commonComponents/CheckBoxComponent/index';
+import { default as DropdownComponent } from './commonComponents/DropdownComponent/index';
+import { default as GridComponent } from './commonComponents/GridComponent/index';
+import { default as MultiSelectComponent } from './commonComponents/MultiSelectComponent/index';
+import { default as TextAreaComponent } from './commonComponents/TextAreaComponent/index';
+import { default as TextBoxComponent } from './commonComponents/TextBoxComponent/index';
 
 export { ToggleComponent, CheckBoxComponent, DropdownComponent, GridComponent, MultiSelectComponent, TextAreaComponent, TextBoxComponent, };
